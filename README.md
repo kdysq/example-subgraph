612ba4b43255c6f5f05914c46a0ac1a384114a17
kdysq <xiaosiyuan0@gmail.com>
2021-11-06 16:25:38 +0800

    feat: test

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Example NEAR Blocks Subgraph
 
 An example to help you get started with NEAR indexing with The Graph. For more information see the docs on https://thegraph.com/docs/supported-networks/near.
+
+## Dev
+
+```
+yarn
+
+yarn codegen
+
+yarn build
+
+export GRAPH_TOKEN=your_access_token_of_graph
+
+yarn deploy:test
+```
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,13 +1,36 @@
-import { near, BigInt } from "@graphprotocol/graph-ts";
-import { BlockEvent } from "../generated/schema";
+import { near, BigInt, Bytes, log } from "@graphprotocol/graph-ts";
+import { BlockAction, BlockEvent } from "../generated/schema";
 
 export function handleBlock(block: near.Block): void {
   const header = block.header;
-  let event = new BlockEvent(header.hash.toHexString());
+  const event = new BlockEvent(header.hash.toHexString());
   event.number = BigInt.fromI32(header.height as i32);
   event.hash = header.hash;
+  event.account = block.author;
   event.timestampNanosec = BigInt.fromU64(header.timestampNanosec);
   event.gasPrice = header.gasPrice;
+  event.save();
+}
 
+export function handleReceipt(receipt: near.ReceiptWithOutcome): void {
+  const actions = receipt.receipt.actions;
+  for (let i = 0; i < actions.length; i++) {
+    handleAction(actions[i], receipt.receipt, receipt.block.header);
+  }
+}
+
+function handleAction(
+  action: near.ActionValue,
+  receipt: near.ActionReceipt,
+  blockHeader: near.BlockHeader
+): void {
+  if (action.kind != near.ActionKind.FUNCTION_CALL) {
+    // FIXME: log not work on dashboard web-ui
+    log.info("Early return: {}", ["Not a function call"]);
+    return;
+  }
+
+  const event = new BlockAction(blockHeader.hash.toHexString());
+  event.methodName = action.toFunctionCall().methodName;
   event.save();
 }
